14ec6305f414d1d9ef90f74c5156ec9d988dfaab
MatheusFV <mfreitasvenosa@gmail.com>
2022-01-12 22:56:34 +0100

    get chat endpoint

--- sync/server.ts
+++ sync/server.ts
@@ -3,23 +3,61 @@ import bodyParser from 'body-parser'
 const server = express()
 const port = 3000
 
-const messages: string[] = []
+interface Message {
+  user: string
+  message: string
+  created_at: number
+}
 
-// parse application/x-www-form-urlencoded
-// server.use(bodyParser.urlencoded({ extended: false }))
+interface Database {
+  chats: {
+    id: string
+    messages: Message[]
+  }[]
+}
 
-// parse application/json
+const database: Database = {
+  chats: [
+    {
+      id: 'futebol',
+      messages: [],
+    },
+    {
+      id: 'cs',
+      messages: [],
+    },
+  ],
+}
+
 server.use(bodyParser.json())
 
 // criar um rota GET que responde com hello world
 server.get('/version', (request, response) => {
-  response.send(messages[0])
+  response.send(`v.0.0.0`)
 })
 
+// buscar todas as mensagens do chat
+server.get('/chat/:id', (request, response) => {
+  const id = request.params.id
+  const chat = database.chats.find((chat) => chat.id === id)
+  response.json(chat)
+})
+
+// Adicionar uma mensagem nova no chat
 server.post('/message', (request, response) => {
   const message = request.body.message
-  messages.push(message)
-  response.send('Message successfully saved! ' + message)
+  const id = request.body.id
+  const user = request.body.user
+
+  const chat = database.chats.find((chat) => chat.id === id)
+  if (chat) {
+    chat.messages.push({
+      user: user,
+      message: message,
+      created_at: new Date().getTime(),
+    })
+  }
+  response.json(chat)
 })
 
 server.listen(port, () => {
